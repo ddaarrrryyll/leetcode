19a60ce1a9c2a8627667dcaa062f33f17777b8be
Darryl Ng <51407026+ddaarrrryyll@users.noreply.github.com>
2021-06-29 16:56:38 +0800

    Time: 176 ms (6.45%), Space: 8.8 MB (75.81%) - LeetHub

---
+++ max-consecutive-ones-iii/max-consecutive-ones-iii.c
@@ -0,0 +1,24 @@
+
+
+int longestOnes(int* nums, int numsSize, int k){
+    // sliding window
+    // [1,1,1,0,0,0,1,1,1,1,0]
+    //            j   
+    //                      i
+    int i = 0;
+    int j = 0;
+    
+    while (i < numsSize) {
+        // decrement k to 'convert' 0 to 1
+        if (nums[i] == 0) k--;
+        // if k < 0 we need to move j
+        if (k < 0) {
+            if (nums[j] == 0) k++;
+            j++;
+        }
+        i++;
+        // i and j is moved together if k < 0 to keep the biggest i - j  value
+        printf("i = %d, j = %d, k = %d\n", i, j, k);
+    }
+    return i - j;
+}
