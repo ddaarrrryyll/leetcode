a393b3b5ef656c19a00ceab47bcf17105a293ba4
Darryl Ng <51407026+ddaarrrryyll@users.noreply.github.com>
2021-09-28 00:28:55 +0800

    Time: 52 ms (72.69%), Space: 14.2 MB (84.63%) - LeetHub

--- unique-email-addresses/unique-email-addresses.py
+++ unique-email-addresses/unique-email-addresses.py
@@ -4,6 +4,7 @@ class Solution:
         for email in emails:
             local, domain  = email.split('@')
             front = local.split('+')[0].replace('.', '')
+            
             # for different cases e.g. Abc and aBC should be the same local name
             l.add(front.lower() + '@' + domain.lower())
         print(l)
