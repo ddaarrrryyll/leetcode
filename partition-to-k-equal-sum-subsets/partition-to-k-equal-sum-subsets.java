4fe1274b14e455a56ff2ca10399c5a3c238a7d62
Darryl Ng <51407026+ddaarrrryyll@users.noreply.github.com>
2021-09-30 16:19:19 +0800

    Time: 11 ms (25.25%), Space: 36.8 MB (42.96%) - LeetHub

---
+++ partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.java
@@ -0,0 +1,30 @@
+import java.util.stream.*;
+
+class Solution {
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        int sum = IntStream.of(nums).sum();
+        System.out.println("each array must have sum: " + sum/k);
+        if (k == 0 || sum % k != 0) return false;
+        else return canPartition(0, nums, new boolean[nums.length], k, 0, sum/k);
+    }
+    
+    public boolean canPartition(int start, int[] nums, boolean[] used, int k, int currSum, int targetSum) {
+        // if one more bucket left means partitioning succeeded
+        if (k==1) return true;
+        // finished one bucket
+        if (currSum == targetSum) return canPartition(0, nums, used, k-1, 0, targetSum);
+        for (int i = start; i < nums.length; i++) {
+            // if element is not used
+            if (!used[i]) {
+                // marked as used
+                used[i] = true;
+                // check if able to partition
+                if (canPartition(i+1, nums, used, k, currSum + nums[i], targetSum)) return true;
+                // if cannot partition, then we dont choose nums[i]
+                used[i] = false;
+            }
+        }
+        // if no returns return false
+        return false;
+    }
+}
