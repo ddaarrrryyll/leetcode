c23d4a3b614495f72b63306b3c966dc20ad2f1d8
Darryl Ng <51407026+ddaarrrryyll@users.noreply.github.com>
2021-11-27 13:11:28 +0800

    Time: 340 ms (5.94%), Space: 20.9 MB (92.67%) - LeetHub

--- product-of-array-except-self/product-of-array-except-self.py
+++ product-of-array-except-self/product-of-array-except-self.py
@@ -7,13 +7,44 @@ class Solution:
         i, temp = 1, 1
         res = [1 for i in range(n)]
         
+        # setting res[i] to multiple of every element infront of it
         for i in range(n):
             res[i] = temp
             temp *= nums[i]
             
         temp = 1
         
+        # multiplying res[i] to every element behind it
         for i in range(n - 1, -1, -1):
             res[i] *= temp
             temp *= nums[i]
         return res
+    
+# prefix sum soultion
+    
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # running product of prefix (front to back)
+        p = list(accumulate(nums, mul))
+        # running product of suffix (back to front, but flipped back after)
+        s = list(accumulate(nums[::-1], mul))[::-1]
+        # print(p)
+        # print(s)
+        
+        out = []
+        for i in range(len(nums)):
+            if i > 0:
+                pre = p[i-1]
+            # if i is first element, prefix should be 1
+            else:
+                pre = 1
+            
+            if i+1 < len(nums):
+                suf = s[i+1]
+            # if i is last element, suffix should be 1
+            else:
+                suf = 1
+                
+            out.append(pre*suf)
+            
+        return out
