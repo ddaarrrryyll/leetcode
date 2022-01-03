be37a841f4c3b2e560966fd75848a0f8131957a2
Darryl Ng <51407026+ddaarrrryyll@users.noreply.github.com>
2022-01-03 13:33:06 +0800

    Time: 1823 ms (6.15%), Space: 14.5 MB (44.89%) - LeetHub

---
+++ first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -0,0 +1,20 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        # the fastest way should be checking bruteforcely and marking if an index has been matched
+        matched = [0] * len(s)
+        for i in range(len(s)):
+            # skip if has been matched 
+            if matched[i]:
+                continue
+            
+            flag = 0
+            for j in range(i+1, len(s)):
+                # if anything after i is matched, we set it in matched array so that we can skip them afterwards
+                if s[i] == s[j]:
+                    matched[j] = 1
+                    flag = 1
+                    
+            if not flag:
+                return i
+            
+        return -1
