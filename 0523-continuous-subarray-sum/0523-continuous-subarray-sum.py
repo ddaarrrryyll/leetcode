9828d668abfd8beb120a206798d1b208d0f3e257
Darryl <51407026+ddaarrrryyll@users.noreply.github.com>
2022-10-26 22:53:16 +0800

    Time: 1092 ms (86.71%), Space: 33.1 MB (73.31%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        
+        hash_map = {0: 0}    
+        curr_sum = 0
+    
+        for i in range(len(nums)):
+            curr_sum += nums[i]
+            
+            # having two same modulo appearing means the sum of them will give 0
+            if curr_sum % k not in hash_map:
+                hash_map[curr_sum % k] = i + 1
+            elif hash_map[curr_sum % k] < i:
+                return True
+        
+        return False
+                
