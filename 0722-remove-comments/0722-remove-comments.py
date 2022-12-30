74c1a5e216c3d813568a3ccff82d2e552e4ad23f
Darryl <51407026+ddaarrrryyll@users.noreply.github.com>
2022-12-30 14:47:08 +0800

    Time: 35 ms (82.09%), Space: 13.9 MB (84.82%) - LeetHub

---
+++ 0722-remove-comments/0722-remove-comments.py
@@ -0,0 +1,24 @@
+class Solution:
+    def removeComments(self, source: List[str]) -> List[str]:
+        out = []
+        buf = ''
+        blockOpen = False
+        for line in source:
+            i = 0
+            while i < len(line):
+                char = line[i]
+                if char == '/' and (i+1) < len(line) and line[i+1] == '/' and not blockOpen:
+                    i = len(line)
+                elif char == '/' and (i+1) < len(line) and line[i+1] == '*' and not blockOpen:
+                    blockOpen = True
+                    i += 1
+                elif char == '*' and (i+1) < len(line) and line[i+1] == '/' and blockOpen:
+                    blockOpen = False
+                    i += 1
+                elif not blockOpen:
+                    buf += char
+                i += 1
+            if buf and not blockOpen:
+                out.append(buf)
+                buf = ''
+        return out 
