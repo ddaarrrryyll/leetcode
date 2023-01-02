4b15763c695563244b05dfe4252f27d6b82fdc61
Darryl <51407026+ddaarrrryyll@users.noreply.github.com>
2023-01-02 15:13:43 +0800

    Time: 826 ms (83.92%), Space: 14.1 MB (36.47%) - LeetHub

--- 1307-verbal-arithmetic-puzzle/1307-verbal-arithmetic-puzzle.py
+++ 1307-verbal-arithmetic-puzzle/1307-verbal-arithmetic-puzzle.py
@@ -1,51 +1,55 @@
 class Solution:
     def isSolvable(self, words: List[str], result: str) -> bool:
+        # combine all the words into one array
         allWords = words + [result]
-        first_char = set()
-        for word in allWords:
-            if len(word) > 1:
-                first_char.add(word[0])
-        # get length of the longest word
+        # collect all the first characters of the words (this is to make sure it starts at 1 when we iterate possibilities)
+        firstChar = set([word[0] for word in allWords if len(word) > 1])
+        # early checking
+        # get the length of the longest word, if result is shorter than it we can return
         n = max(map(len, allWords))
-        # if result is shorter than longest word we can stop
         if len(result) < n: return False
         
-        def dfs(charIdx, wordIdx, carry, visited, c2d):
-            # we are traversing top to bottom, right to left
-            
-            # if we reach the last char in the longest word carry should be 0 because it wont be last char otherwise
+        # we traverse top to bottom, for right to left col
+        def backtrack(charIdx, wordIdx, carry, visited, c2d):
+            # we use word[-charIdx-1] to access each char right to left
+            # basecase: reaching first char of longest word
             if charIdx == n: return carry == 0
-            # if we reach the final word; we are traversing from the right to left with -charIdx-1
+            
+            # if we finished processing
             if wordIdx == len(allWords):
-                # check final status for current digit
-                sums = sum(c2d[word[-charIdx - 1]] if charIdx < len(word) else 0 for word in words) + carry
-                
-                if sums % 10 == c2d[result[-charIdx - 1]]:
-                    # check next char (left of curr)
-                    return dfs(charIdx + 1, 0, sums//10, visited, c2d)
-                else:
+                sums = 0
+                # sum up all the chars in that word
+                for word in words:
+                    if charIdx < len(word):
+                        sums += c2d[word[-charIdx-1]]
+                sums += carry
+                # if this sum % 10 does not give result[-charIdx-1], it is wrong
+                if sums % 10 != c2d[result[-charIdx-1]]:
                     return False
-            
-            # if current word length is too short we go to next word
+                else:
+                    return backtrack(charIdx+1, 0, sums//10, visited, c2d)
+            # to check if we have more chars to add
             if wordIdx < len(words) and charIdx >= len(words[wordIdx]):
-                return dfs(charIdx, wordIdx + 1, carry, visited, c2d)
-            
-            # current char
+                return backtrack(charIdx, wordIdx+1, carry, visited, c2d)
+                
+            # mapping of chars
             c = allWords[wordIdx][-charIdx-1]
-            # if char is mapped we can go to next word
             if c in c2d:
-                return dfs(charIdx, wordIdx+1, carry, visited, c2d)
+                return backtrack(charIdx, wordIdx+1, carry, visited, c2d)
             else:
-                # if c is the left most char, it must be 1
-                firstDigit = 1 if c in first_char else 0
-                for digit in range(firstDigit, 10):
+                start = 0 
+                if c in firstChar:
+                    start = 1
+                for digit in range(start, 10):
                     if digit not in visited:
                         visited.add(digit)
                         c2d[c] = digit
-                        if dfs(charIdx, wordIdx + 1, carry, visited, c2d.copy()): return True
-                        # if wrong mapping, remove
+                        if backtrack(charIdx, wordIdx+1, carry, visited, c2d.copy()): return True
                         else:
                             visited.remove(digit)
                 return False
+            
         
-        return dfs(0, 0, 0, set(), {})
+        return backtrack(0, 0, 0, set(), {})
+        
+        
