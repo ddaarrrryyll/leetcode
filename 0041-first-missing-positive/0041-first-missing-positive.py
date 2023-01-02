df330539fba0950c2e022950451fa8834ebbaffb
Darryl <51407026+ddaarrrryyll@users.noreply.github.com>
2023-01-02 14:20:26 +0800

    Time: 289 ms (86.61%), Space: 27.1 MB (85.93%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,31 +1,20 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        # # with linear space
-        # temp = defaultdict(lambda: None)
-        # for idx, num in enumerate(nums):
-        #     if num > 0:
-        #         temp[num] = 1
-        # print(temp)
-        # i = 1
-        # while temp[i]:
-        #     i += 1
-        # return i
-    
-        # with constant space
-        for i in range(len(nums)):
-            if nums[i] < 1 or nums[i] > len(nums):
+        # we can use len(nums)+1 as a flag and mod nums[i] to get the og value back
+        # cleaning
+        n = len(nums)
+        for i in range(n):
+            if nums[i] < 1 or nums[i] > n:
                 nums[i] = 0
         
-        for i in range(len(nums)):
-            # get back the original nums[i] value
-            og = nums[i] % (len(nums)+1)
-            # if this og is valid, we tag the previous with (len(nums)+1)
-            if 1 <= og <= len(nums):
-                nums[og - 1] += len(nums) + 1
+        # we are using the index as the positive value to return
+        for i in range(n):
+            og = nums[i] % (n+1)
+            if 0 < og <= n:
+                nums[og-1] += n+1
         
-        # if nums[i] <= len(nums), means we never added len(nums)+1 to it aka did not meet it's +1
-        for i in range(len(nums)):
-            if nums[i] <= len(nums):
+        for i in range(n):
+            if nums[i] < n+1:
                 return i+1
         
         return len(nums)+1
